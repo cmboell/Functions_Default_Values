36265cd43e6b5ba5a4596fd36f3a61b2b33f9b77
Colby Boell <68253967+cmboell@users.noreply.github.com>
2022-02-20 11:46:53 -0600

    minor note changes

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -4,8 +4,8 @@ Program: validate_input_in_functions.py
 Author: Colby Boell
 Last date modified: 02/20/2022
 
-The purpose of this program is to use a function score_input() taht takes parameters test_name, test_score,
-and invalid_message that validates the test_score, then returns a string.
+The purpose of this program is to use a function score_input() that takes parameters test_name, test_score,
+and invalid_message that validates the test_score, then returns a string. Validates test score as well.i
 """
 
 
