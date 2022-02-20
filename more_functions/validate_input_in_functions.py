af1246388fc7f2422e725772389a01c37e82467a
Colby Boell <68253967+cmboell@users.noreply.github.com>
2022-02-20 11:07:19 -0600

    initial commit

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,9 @@
+"""
+Assignment: Functions Default Values Assignment
+Program: validate_input_in_functions.py
+Author: Colby Boell
+Last date modified: 02/20/2022
+
+The purpose of this program is to use a function score_input() taht takes parameters test_name, test_score,
+and invalid_message that validates the test_score, then returns a string.
+"""
